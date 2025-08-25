f4b0d6bebe2439a89259deb2facc2734563b3743
Eduardo Franco de Lima <edu.developer@outlook.com>
2025-08-25 10:33:17 -0300

    #00003 - Ajuste no response do cadastro de produtos

--- API.Lanchonete.Domain/DTO/Response/ProdutoCadastroResponseDto.cs
+++ API.Lanchonete.Domain/DTO/Response/ProdutoCadastroResponseDto.cs
@@ -5,5 +5,7 @@ namespace API.Lanchonete.Domain.DTO.Response
     public class ProdutoCadastroResponseDto : ProdutoBase
     {
         public int IdProduto { get; set; }
+        public DateTime? DataCadastro { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
     }
 }
